f1b0dee0eb70b37cf80d9ec3b8c9e15868573971
Alex Allenspach <61338786+alexallenspach@users.noreply.github.com>
2020-10-11 17:43:19 -0600

    Create Use vapply (2).R

---
+++ the apply family/Use vapply (2).R
@@ -0,0 +1,14 @@
+# So far you've seen that vapply() mimics the behavior of sapply() if everything goes according to plan. But what if it doesn't?
+
+# In the video, Filip showed you that there are cases where the structure of the output of the function you want to apply, FUN, does not correspond to the template 
+# you specify in FUN.VALUE. In that case, vapply() will throw an error that informs you about the misalignment between expected and actual output.
+
+# temp is already available in the workspace
+
+# Definition of the basics() function
+basics <- function(x) {
+  c(min = min(x), mean = mean(x), median = median(x), max = max(x))
+}
+
+# Fix the error:
+vapply(temp, basics, numeric(3)) # CHANGE 3 TO 4 
