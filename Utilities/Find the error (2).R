3052e29b9c889d0546ee88cd6efbbfeec7a11317
Alex Allenspach <61338786+alexallenspach@users.noreply.github.com>
2020-10-11 18:20:19 -0600

    Create Find the error (2).R

---
+++ Utilities/Find the error (2).R
@@ -0,0 +1,7 @@
+Just as before, let's switch roles. It's up to you to see what unforgivable mistakes we've made. Go fix them!
+
+# Fix me
+seq(rep(1, 7, by = 2), times = 7)
+
+# Fixed code
+rep(seq(1, 7, by = 2), times = 7)
